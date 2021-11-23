fb0fc69bf01dab5e083f756f8fd81dcb294cb579
lester <a1703583@idat.edu.pe>
2021-11-23 11:21:25 -0500

    3.- Property Binding & Event Binding

--- src/app/secretaria/secretaria.component.ts
+++ src/app/secretaria/secretaria.component.ts
@@ -16,6 +16,39 @@ export class SecretariaComponent implements OnInit {
   llamaEmpresa(value:String){
 
   }
+
+
+  //CREANDO LOS PROPERTY BINDING
+
+  habilitarCuadroTexto=false;
+
+  usuarioRegistrado=false;
+
+  textoDeRegistro="No hay nadie registrado";
+
+  getRegistroUsuario(){
+
+    this.usuarioRegistrado=false;
+  }
+
+
+  //CREANDO EVENT BINDING...mostrará una ventana emergente
+
+  setusuarioRegistrado(event:Event){
+   // alert("El usuario ya esta registrado")
+
+   //this.textoDeRegistro="El usuaro ya esta registrado"
+
+   if((<HTMLInputElement>event.target).value=="si"){
+
+    this.textoDeRegistro="El usuario ya esta registrado";
+   }else{
+     this.textoDeRegistro="No hay nadieregistrado";
+   }
+
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
